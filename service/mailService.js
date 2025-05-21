048a0d559b2ea1af1b69f19b99aaf96e14eef5fd
rizkirenden <rizkirenden@gmail.com>
2025-05-21 15:12:30 +0800

    selesai

--- service/mailService.js
+++ service/mailService.js
@@ -4,15 +4,15 @@ async function sendVerificationEmail(email, token) {
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: "rizkirenden@gmail.com",
-      pass: "vnes dbgd peri bicg",
+      user: "",
+      pass: "",
     },
   });
 
   const verificationLink = `http://localhost:3000/verify-email?token=${token}`;
 
   await transporter.sendMail({
-    from: '"Aplikasi Anda" <rizkirenden@gmail.com>',
+    from: '"Movie App" <rizkirenden@gmail.com>',
     to: email,
     subject: "Verifikasi Email",
     html: `<p>Silakan klik link berikut untuk verifikasi email:</p><a href="${verificationLink}">${verificationLink}</a>`,
